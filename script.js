f934338233bdebd9bdfb096869a87c76eb08d629
wisdo-coder <wisdomkelechi46@gmail.com>
2025-08-11 21:29:37 +0100

    Day 3: Added CSS styling for cleaner look

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello from My App!");
+// Get elements from the HTML
+const message = document.getElementById("message");
+const button = document.getElementById("changeBtn");
+
+// Add a click event to the button
+button.addEventListener("click", function() {
+    message.textContent = "You clicked the button! 🎉";
+});
